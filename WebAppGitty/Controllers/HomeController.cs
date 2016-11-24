ca19895e663c77888fafd8a780123b9e66b64a5d
Aiion <alex.soderberg97@gmail.com>
2016-11-24 13:48:19 +0100

    Nu fungerar allt

--- WebAppGitty/Controllers/HomeController.cs
+++ WebAppGitty/Controllers/HomeController.cs
@@ -3,15 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using WebAppGitty.Models;
 
 namespace WebAppGitty.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+
+        private CloudStorageAccount storageAccount;
+        private CloudTableClient tableClient;
+        private CloudTable table;
+        public HomeController()
+        {
+
+            storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=webappgitty;AccountKey=SiHK3hNYpSYqTcCyScNWem9EvLEJg0CtWehoj4RfLxJMtwIcxtTgrGY67WQxcsxVuHPRObZYlBd0x8cevDd9VQ==");
+            tableClient = storageAccount.CreateCloudTableClient();
+            table = tableClient.GetTableReference("msgInfo");
+            table.CreateIfNotExists();
+
+        }
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult MSG()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult MSG(Class1 xd)
+        {
+            Class1 entity = new Class1("abc", Guid.NewGuid().ToString()) { Name = xd.Name, MyMessage = xd.MyMessage, Email = xd.Email };
+
+            TableOperation insertOperation = TableOperation.Insert(entity);
+            table.Execute(insertOperation);
+
+            return View("Index");
         }
     }
 }
---
+++ WebAppGitty/Models/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace WebAppGitty.Models
+{
+    public class Class1 : TableEntity
+    {
+        // public Class1(string category, string sku):base(category, sku)
+        //{}
+        public Class1() { }
+
+        public Class1(string partitionKey, string rowKey) : base(partitionKey, rowKey)
+        {
+            this.PartitionKey = partitionKey;
+            this.RowKey = rowKey;
+
+        }
+
+
+       // public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string MyMessage { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
